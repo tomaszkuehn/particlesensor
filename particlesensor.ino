1e210800dc69afe8d274a9f9665d000fe350e332
tomaszkuehn <tomasz.kuehn@gmail.com>
2020-08-12 16:53:01 +0200

    Restart by button

--- particlesensor.ino
+++ particlesensor.ino
@@ -4,8 +4,8 @@ hw_timer_t * timer = NULL;
 int _pm1;
 int _pm25;
 int _pm10;
-int _counter = 0;
-int _intcount = 0;
+int _counter = 0;  // counts number of measurements
+int _intcount = 0; // counts interrupts
 
 /* Interrupt routine */
 void serialRX() {
@@ -79,6 +79,8 @@ class particlesensor {
       c1 = _intcount;
       usleep(100);
     }
+    //M5.Lcd.setCursor(100, 50);
+    //M5.Lcd.printf("%d", c1);
     pm1  = _pm1;
     pm25 = _pm25;
     pm10 = _pm10;
@@ -86,13 +88,15 @@ class particlesensor {
     return counter > 10;
   }
   void sleep() {
-    Serial2.write(0x42);
-    Serial2.write(0x4d);  
-    Serial2.write(0xe4);
-    Serial2.write(0x00);
-    Serial2.write(0x00);
-    Serial2.write(0x01);
-    Serial2.write(0x73);
+    if(sleepstatus == 0) {
+      Serial2.write(0x42);
+      Serial2.write(0x4d);  
+      Serial2.write(0xe4);
+      Serial2.write(0x00);
+      Serial2.write(0x00);
+      Serial2.write(0x01);
+      Serial2.write(0x73);
+    }
     sleepstatus = 1;
   }
   void start() {
@@ -104,6 +108,7 @@ class particlesensor {
     Serial2.write(0x01);
     Serial2.write(0x74); 
     sleepstatus = 0;
+    _counter = 0;
   }
 };
 
@@ -111,26 +116,34 @@ particlesensor ps;
 
 void setup() {
   M5.begin();
+  M5.Power.begin();
   M5.Lcd.setTextColor(WHITE);
   M5.Lcd.setTextSize(2);
   M5.Lcd.printf("Particle sensor");
   Serial2.begin(9600, SERIAL_8N1, 16, 17);
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &serialRX, true);
-  timerAlarmWrite(timer, 500, true);//usec
+  timerAlarmWrite(timer, 5000, true); // usec
   timerAlarmEnable(timer);
   ps.start();
+  pinMode(39, INPUT); // button A
+  M5.Lcd.setCursor(25, 225);
+  M5.Lcd.printf("RESTART");
 }
 
 
 
 void loop() {
-  sleep(2);
-  ps.read();
+  int btnA = digitalRead(39);
+  if (btnA == 0) {
+    ps.start();
+  }
+  sleep(1);
   M5.Lcd.setCursor(10, 50);
   M5.Lcd.fillRect(0, 40, 320, 120, BLACK);
+  ps.read();
   M5.Lcd.setCursor(10, 50);
-  M5.Lcd.printf("# %3d", ps.counter);
+  M5.Lcd.printf("# %d ", ps.counter);
   M5.Lcd.setCursor(10, 70);  
   M5.Lcd.printf("PM1.0 %4d.%1d", ps.pm1/10, ps.pm1%10);
   M5.Lcd.setCursor(10, 90);
